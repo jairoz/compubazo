8234c11e6c3542ffaaa1fff8b83c607d618da76c
jairoz <a20122093@pucp.pe>
2016-03-18 12:36:33 -0500

    ahora estan los pasos para subir archivos con commit

--- gutenacht.C
+++ gutenacht.C
@@ -1,1 +1,12 @@
 Este archivo dice gute nacht :D por segunda vez :O
+recordar
+//para agregar el archivo
+git add -A
+//para agregar comentario
+git commit -m "Ingrese comentario"
+//para ver los commits
+git log 
+//para subir
+git push
+//para ver los cambios
+git status
